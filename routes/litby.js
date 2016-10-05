743280161df16e353bd0814738da642ba8b3d2df
Imran <imran@loveairbear.com>
2016-10-04 20:14:23 -0400

    small errors

--- routes/litby.js
+++ routes/litby.js
@@ -88,7 +88,7 @@ router.get('/webhook', function(req, res, next) {
         "buttons": [
           {
             "type": "web_url",
-            "url": 'https://stansonweb.herokuapp.com/geteditor?fbid='+req.query.fbid,
+            "url": 'https://stansonweb.herokuapp.com/litby/geteditor?fbid='+req.query.fbid,
             "title": "View in editor"
           }
         ]
